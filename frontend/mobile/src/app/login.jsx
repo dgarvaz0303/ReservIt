93c18257b9fc71812029e8d582852cd73af8c9f9
DanielGarciaV <garciavd030@gmail.com>
2026-04-08 13:54:25 +0200

    Autentificacion y mantenimiento de sesion

--- frontend/mobile/src/app/login.jsx
+++ frontend/mobile/src/app/login.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -10,7 +12,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://IP/api/login", {
+      const res = await fetch("http://192.168.1.132:8000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -27,7 +29,13 @@ export default function Login() {
         throw new Error(data.detail || "Error en login");
       }
 
+      // GUARDAR TOKEN
+      await AsyncStorage.setItem("token", data.access_token);
+
       console.log("Login OK:", data);
+
+      // REDIRIGIR
+      router.replace("/home");
 
     } catch (err) {
       setError(err.message);
