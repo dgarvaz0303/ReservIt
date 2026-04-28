84f1b550f39022b843bd1479ee1b3b2eab374d16
DanielGarciaV <garciavd030@gmail.com>
2026-04-28 23:07:38 +0200

    rediccionamiento funcional

--- frontend/mobile/app/(tabs)/_layout.tsx
+++ frontend/mobile/app/(tabs)/_layout.tsx
@@ -1,43 +1,54 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs, router } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import MobileNavbar from "@/components/MobileNavbar";
+import MobileFooter from "@/components/MobileFooter";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const [loading, setLoading] = useState(true);
+  const [rol, setRol] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await AsyncStorage.getItem("token");
+      const storedRol = await AsyncStorage.getItem("rol");
+
+      console.log("TOKEN:", token);
+      console.log("ROL:", storedRol);
+
+      if (!token || !storedRol) {
+        router.replace("/login");
+        return;
+      }
+
+      setRol(storedRol);
+      setLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading || !rol) {
+    return <Text>Cargando...</Text>;
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
+    <View style={{ flex: 1 }}>
 
       
-    </Tabs>
+      <MobileNavbar rol={rol} />
+
+      <View style={{ flex: 1 }}>
+        <Tabs screenOptions={{ headerShown: false }}>
+          <Tabs.Screen name="index" />
+          <Tabs.Screen name="explore" />
+        </Tabs>
+      </View>
+
+      <MobileFooter />
+
+    </View>
   );
 }
